526820b3d8bc66ad377a0fa6a4b3136f2a4a73e8
Kuchiki Renji <KuchikiRenji@outlook.com>
2022-11-02 17:30:39 +0700

    chore: Change rustfmt and re-fommat codes

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,19 @@ mod git;
 async fn main() {
     let endpoint = "https://api.github.com/graphql";
     let mut headers = HashMap::new();
-    headers.insert("authorization", "Bearer REMOVED_TOKEN");
+    headers.insert(
+        "authorization",
+        "Bearer REMOVED_TOKEN",
+    );
     headers.insert("user-agent", "PostmanRuntime/7.29.2");
-    let client = gql_client::Client::new_with_headers(endpoint, headers);
+    let client =
+        gql_client::Client::new_with_headers(endpoint, headers);
 
-    Git::get_latest_commit_by_repo(client, "git-stats-bot", "maemreyo", 5).await;
+    Git::get_latest_commit_by_repo(
+        client,
+        "git-stats-bot",
+        "maemreyo",
+        5,
+    )
+    .await;
 }
